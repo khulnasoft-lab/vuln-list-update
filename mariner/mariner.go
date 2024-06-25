aede2866e8a0b29a3d067e34c1bddcc3d896090c
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-06-25 13:07:57 +0600

    Update mariner.go
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- mariner/mariner.go
+++ mariner/mariner.go
@@ -1,199 +1,134 @@
-package mariner
+package mariner_test
 
 import (
-	"context"
-	"encoding/xml"
-	"errors"
-	"fmt"
-	"log"
+	"io/fs"
 	"os"
 	"path/filepath"
-	"strings"
+	"testing"
 
-	"github.com/cheggaaa/pb"
-	"github.com/khulnasoft-lab/vuln-list-update/utils"
-	"golang.org/x/xerrors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/khulnasoft-lab/vuln-list-update/mariner"
 )
 
-const (
-	repoURL = "https://github.com/microsoft/AzureLinuxVulnerabilityData/archive/refs/heads/main.tar.gz//AzureLinuxVulnerabilityData-main"
-	cblDir  = "mariner" // CBL-Mariner Vulnerability Data
-	retry   = 3
+func TestUpdate(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputFile string
+		wantErr   string
+	}{
+		{
+			name:      "happy path",
+			inputFile: "file::testdata/happy",
+		},
+		{
+			name:      "sad path, invalid xml",
+			inputFile: "file::testdata/sad",
+			wantErr:   "failed to decode xml",
+		},
+	}
 
-	testsDir       = "tests"
-	objectsDir     = "objects"
-	statesDir      = "states"
-	definitionsDir = "definitions"
-)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tmpDir := t.TempDir()
+			cc := mariner.NewConfig(mariner.WithURL(tt.inputFile), mariner.WithDir(tmpDir), mariner.WithRetry(0))
 
-var (
-	ErrInvalidCVEFormat = errors.New("invalid CVE-ID format")
-	ErrNonCVEID         = errors.New("discovered non-CVE-ID")
-)
+			err := cc.Update()
+			if tt.wantErr != "" {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tt.wantErr)
+				return
+			}
+			require.NoError(t, err)
 
-type Config struct {
-	*options
+			err = filepath.WalkDir(tmpDir, func(path string, d fs.DirEntry, err error) error {
+				require.NoError(t, err, tt.name)
+				if !d.Type().IsRegular() {
+					return nil
+				}
+
+				got, err := os.ReadFile(path)
+				require.NoError(t, err, path)
+
+				rel, err := filepath.Rel(tmpDir, path)
+				require.NoError(t, err, path)
+
+				goldenPath := filepath.Join("testdata", "golden", "mariner", rel)
+				want, err := os.ReadFile(goldenPath)
+				require.NoError(t, err, goldenPath)
+
+				assert.JSONEq(t, string(want), string(got), path)
+
+				return nil
+			})
+			require.NoError(t, err, tt.name)
+		})
+	}
 }
 
-type option func(*options)
-
-type options struct {
-	url   string
-	dir   string
-	retry int
-}
-
-func WithURL(url string) option {
-	return func(opts *options) { opts.url = url }
-}
-
-func WithDir(dir string) option {
-	return func(opts *options) { opts.dir = dir }
-}
-
-func WithRetry(retry int) option {
-	return func(opts *options) { opts.retry = retry }
-}
-
-func NewConfig(opts ...option) Config {
-	o := &options{
-		url:   repoURL,
-		dir:   filepath.Join(utils.VulnListDir(), cblDir),
-		retry: retry,
+func TestAdvisoryID(t *testing.T) {
+	tests := []struct {
+		name string
+		def  mariner.Definition
+		want string
+	}{
+		{
+			name: "advisory_id without version",
+			def: mariner.Definition{
+				Metadata: mariner.Metadata{
+					AdvisoryID: "1111",
+				},
+			},
+			want: "1111",
+		},
+		{
+			name: "advisory_id with version",
+			def: mariner.Definition{
+				Metadata: mariner.Metadata{
+					AdvisoryID: "1111-2",
+				},
+			},
+			want: "1111-2",
+		},
+		{
+			name: "build advisoryID converting long version to 1",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "2000000001",
+			},
+			want: "27423-1",
+		},
+		{
+			name: "build advisoryID converting long version to 0",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "2000000000",
+			},
+			want: "27423",
+		},
+		{
+			name: "build advisoryID with short 1 version",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "1",
+			},
+			want: "27423-1",
+		},
+		{
+			name: "build advisoryID with short 0 version",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "0",
+			},
+			want: "27423",
+		},
 	}
 
-	for _, opt := range opts {
-		opt(o)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := mariner.AdvisoryID(tt.def)
+			require.Equal(t, tt.want, got)
+		})
 	}
-
-	return Config{
-		options: o,
-	}
-}
-
-func (c Config) Update() error {
-	ctx := context.Background()
-
-	log.Printf("Remove CBL-Mariner Vulnerability Data directory %sn", c.dir)
-	if err := os.RemoveAll(c.dir); err != nil {
-		return xerrors.Errorf("failed to remove CBL-Mariner Vulnerability Data directory: %w", err)
-	}
-
-	log.Print("Fetching CBL-Mariner Vulnerability Data")
-	tmpDir, err := utils.DownloadToTempDir(ctx, c.url)
-	if err != nil {
-		return xerrors.Errorf("failed to retrieve CBL-Mariner Vulnerability Data: %w", err)
-	}
-	defer os.RemoveAll(tmpDir)
-
-	entries, err := os.ReadDir(tmpDir)
-	if err != nil {
-		return xerrors.Errorf("failed to read directory: %w", err)
-	}
-
-	for _, entry := range entries {
-		if entry.IsDir() {
-			continue
-		}
-
-		if !strings.HasPrefix(entry.Name(), "cbl-mariner-") {
-			continue
-		}
-		if filepath.Ext(entry.Name()) != ".xml" {
-			continue
-		}
-
-		osVersoin := strings.TrimSuffix(strings.TrimSuffix(strings.TrimPrefix(entry.Name(), "cbl-mariner-"), "-oval.xml"), "-preview")
-		if err := c.update(osVersoin, filepath.Join(tmpDir, entry.Name())); err != nil {
-			return xerrors.Errorf("failed to update oval data: %w", err)
-		}
-	}
-	return nil
-}
-
-func (c Config) update(version, path string) error {
-	f, err := os.Open(path)
-	if err != nil {
-		return xerrors.Errorf("failed to open file: %w", err)
-	}
-
-	var oval OvalDefinitions
-	if err := xml.NewDecoder(f).Decode(&oval); err != nil {
-		return xerrors.Errorf("failed to decode xml: %w", err)
-	}
-	dirPath := filepath.Join(c.dir, version)
-
-	// write tests/tests.json file
-	if err := utils.Write(filepath.Join(dirPath, testsDir, "tests.json"), oval.Tests); err != nil {
-		return xerrors.Errorf("failed to write tests: %w", err)
-	}
-
-	// write objects/objects.json file
-	if err := utils.Write(filepath.Join(dirPath, objectsDir, "objects.json"), oval.Objects); err != nil {
-		return xerrors.Errorf("failed to write objects: %w", err)
-	}
-
-	// write states/states.json file
-	if err := utils.Write(filepath.Join(dirPath, statesDir, "states.json"), oval.States); err != nil {
-		return xerrors.Errorf("failed to write states: %w", err)
-	}
-
-	// write definitions
-	bar := pb.StartNew(len(oval.Definitions.Definition))
-	for _, def := range oval.Definitions.Definition {
-		if err := c.saveAdvisoryPerYear(filepath.Join(dirPath, definitionsDir), def); err != nil {
-			return xerrors.Errorf("failed to save advisory per year: %w", err)
-		}
-
-		bar.Increment()
-	}
-	bar.Finish()
-
-	return nil
-}
-func (c Config) saveAdvisoryPerYear(dirName string, def Definition) error {
-	// Use advisory_id for file name to avoid overwriting files when there are 2 definitions for same CVE
-	// cf. https://github.com/aquasecurity/trivy-db/issues/379
-	fileName := fmt.Sprintf("%s.json", AdvisoryID(def))
-
-	vulnID := def.Metadata.Reference.RefID
-	if !strings.HasPrefix(vulnID, "CVE") {
-		log.Printf("discovered non-CVE-ID: %s", vulnID)
-		return ErrNonCVEID
-	}
-
-	s := strings.Split(vulnID, "-")
-	if len(s) != 3 {
-		log.Printf("invalid CVE-ID format: %s", vulnID)
-		return ErrInvalidCVEFormat
-	}
-
-	yearDir := filepath.Join(dirName, s[1])
-	if err := utils.Write(filepath.Join(yearDir, fileName), def); err != nil {
-		return xerrors.Errorf("unable to write a JSON file: %w", err)
-	}
-	return nil
-}
-
-// AdvisoryID returns advisoryID for Definition.
-// If `advisory_id` field does not exist, create this field yourself using the Azure Linux format.
-//
-// Azure Linux uses `<number_after_last_colon_from_id>-<last_number_from_version>` format for `advisory_id`.
-// cf. https://github.com/aquasecurity/vuln-list-update/pull/271#issuecomment-2111678641
-// e.g.
-//   - `id="oval:com.microsoft.cbl-mariner:def:27423" version="2000000001"` => `27423-1`
-//   - `id="oval:com.microsoft.cbl-mariner:def:11073" version="2000000000"` => `11073`
-//   - `id="oval:com.microsoft.cbl-mariner:def:6343" version="1"` => `6343-1`
-//   - `id="oval:com.microsoft.cbl-mariner:def:6356" version="0"` => `6356`
-func AdvisoryID(def Definition) string {
-	id := def.Metadata.AdvisoryID
-	if id == "" {
-		ss := strings.Split(def.ID, ":")
-		id = ss[len(ss)-1]
-		// for `0` versions `-0` suffix is omitted.
-		if def.Version != "" && def.Version[len(def.Version)-1:] != "0" {
-			id = fmt.Sprintf("%s-%s", id, def.Version[len(def.Version)-1:])
-		}
-	}
-	return id
 }
