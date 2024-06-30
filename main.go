88c033a211e3368ca1bec2856de1da31f8f43ea7
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-06-30 19:06:13 +0600

    Update main.go
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ import (
 
 var (
 	target = flag.String("target", "", "update target (nvd, alpine, alpine-unfixed, redhat, redhat-oval, "+
-		"debian, ubuntu, amazon, oracle-oval, suse-cvrf, photon, arch-linux, ghsa, glad, cwe, osv, mariner, kevc, wolfi, chainguard, k8s)")
+		"debian, ubuntu, amazon, oracle-oval, suse-cvrf, photon, arch-linux, ghsa, glad, cwe, osv, mariner, kevc, wolfi, chainguard, k8s, openeuler)")
 	vulnListDir  = flag.String("vuln-list-dir", "", "vuln-list dir")
 	targetUri    = flag.String("target-uri", "", "alternative repository URI (only glad)")
 	targetBranch = flag.String("target-branch", "", "alternative repository branch (only glad)")
@@ -174,6 +174,11 @@ func run() error {
 		ku := k8s.NewUpdater()
 		if err := ku.Update(); err != nil {
 			return xerrors.Errorf("k8s update error: %w", err)
+		}
+	case "openeuler":
+		ec := openeuler.NewConfig()
+		if err := ec.Update(); err != nil {
+			return xerrors.Errorf("openEuler CVE update error: %w", err)
 		}
 	default:
 		return xerrors.New("unknown target")
